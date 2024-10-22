fb0610ee60db83469247866d7dbc92853817f6a4
foxmentor240 <Foxminded@788>
2024-10-22 22:44:15 +0300

    apply wo error

---  wordpress.tf
+++  wordpress.tf
@@ -1,39 +1,9 @@
-# Load Balancer
-resource "aws_lb" "wordpress" {
-  name               = "wordpress-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups = [aws_security_group.www.id]
-  subnets = [aws_subnet.public1.id, aws_subnet.public2.id]
-}
-
-# Target Group
-resource "aws_lb_target_group" "wordpress" {
-  name        = "wordpress-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "instance"
-}
-
-# Listener
-resource "aws_lb_listener" "wordpress" {
-  load_balancer_arn = aws_lb.wordpress.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.wordpress.arn
-  }
-}
-
 # EC2 Instance
 resource "aws_instance" "wordpress1" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-00385a401487aefa4"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public1.id
-  security_groups = [aws_security_group.www.name]
+  vpc_security_group_ids = [aws_security_group.www.id]
 
   tags = {
     Name = "WordPress1"
@@ -44,35 +14,4 @@ resource "aws_instance" "wordpress1" {
   lifecycle {
     create_before_destroy = true
   }
-}
-
-resource "aws_instance" "wordpress2" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public2.id
-  security_groups = [aws_security_group.www.name]
-
-  tags = {
-    Name = "WordPress2"
-  }
-
-  user_data = file("setup_wordpress.sh")
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Attach EC2 instance to Target Group
-resource "aws_lb_target_group_attachment" "wordpress1" {
-  target_group_arn = aws_lb_target_group.wordpress.arn
-  target_id        = aws_instance.wordpress1.id
-  port             = 80
-}
-
-# Attach EC2 instance to Target Group
-resource "aws_lb_target_group_attachment" "wordpress2" {
-  target_group_arn = aws_lb_target_group.wordpress.arn
-  target_id        = aws_instance.wordpress2.id
-  port             = 80
 }
--- main.tf
+++ main.tf
@@ -11,19 +11,18 @@ resource "aws_subnet" "public1" {
   map_public_ip_on_launch = true
 }
 
-# Public Subnet for EC2
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.2.0/24"
-  availability_zone       = "eu-west-1b"
-  map_public_ip_on_launch = true
+# Private Subnet for RDS and Redis
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.10.0/24"
+  availability_zone = "eu-west-1a"
 }
 
-# Private Subnet for RDS and Redis
-resource "aws_subnet" "private" {
+# Additional Private Subnet for RDS and Redis
+resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.2.0/24"
-  availability_zone = "eu-west-1a"
+  cidr_block        = "192.168.11.0/24"
+  availability_zone = "eu-west-1b"
 }
 
 # Security Group for EC2
@@ -84,7 +83,8 @@ resource "aws_security_group" "db" {
 resource "aws_db_instance" "mysql" {
   identifier           = "wordpress-mysql"
   engine               = "mysql"
-  instance_class       = "db.t2.micro"
+  engine_version       = "8.0.39"
+  instance_class       = "db.t3.micro"
   allocated_storage    = 20
   db_name              = "wordpress"
   username             = "admin"
@@ -104,7 +104,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis7"
   security_group_ids = [aws_security_group.db.id]
   subnet_group_name    = aws_elasticache_subnet_group.main.name
 }
@@ -112,14 +112,15 @@ resource "aws_elasticache_cluster" "redis" {
 output "redis_endpoint" {
   value = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
+
 # DB Subnet Group
 resource "aws_db_subnet_group" "main" {
-  name = "main"
-  subnet_ids = [aws_subnet.private.id]
+  name       = "main"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
 }
 
 # ElastiCache Subnet Group
 resource "aws_elasticache_subnet_group" "main" {
   name = "main"
-  subnet_ids = [aws_subnet.private.id]
+  subnet_ids = [aws_subnet.private1.id]
 }
