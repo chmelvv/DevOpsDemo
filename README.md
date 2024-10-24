560301650719ee54c32a7622857519a0b503f1ee
foxmentor240 <Foxminded@788>
2024-10-24 03:10:51 +0300

    add readme and make project more modular

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Configuration options for deployment script and Terraform module and how
+   to customize them.
+To clone this project to you local machine, run the following command:
+```git clone https://github.com/chmelvv/abz-test.git
+2. Troubleshooting tips and common issues.
---
+++ routes.tf
@@ -0,0 +1,20 @@
+# Create Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+# Associate Route Table with Public Subnet
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
